d8171b7b778aa4ad3a9069eb5e0d15f412c6f64e
HatingLife576 <42309277+HatingLife576@users.noreply.github.com>
2018-11-21 00:56:06 -0700

    updated the bot.py

--- bot.py
+++ bot.py
@@ -18,14 +18,11 @@ async def on_message(message):
     if message.content.upper().startswith(cp + 'PING'):
         userID = message.author.id
         await client.send_message(message.channel, "<@%s> pongo!" % (userID))
-
-
-
-
-
-
-
-
+    if message.content.upper().startswith(cp + 'SAY'):
+        args =message.content.split(" ")
+        #args[0] = cp + 'SAY'
+        #args[1:] = %s
+        await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
 
 
 
